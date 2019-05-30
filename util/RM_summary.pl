a6b95f2da020aebdb11deb4ad7c16b8b0a2abfb6
Shujun Ou <shujun.ou.1@gmail.com>
2019-05-30 15:39:24 -0500

    update hard-coded paths to relative paths

--- util/RM_summary.pl
+++ util/RM_summary.pl
@@ -1,29 +1,33 @@
 #!/usr/bin/perl -w
 use strict;
+use FindBin;
 
-my $usage = "";
+my $usage = "\nSummarize RepeatMasker .out file results based on the genome name\n
+	perl RM_summary.pl genome.fa\n";
+
 my $genome = $ARGV[0];
+my $script_path = $FindBin::Bin;
 
 die "No fasta specified!\n$usage" unless -s $genome;
 die "No RM files associated with the $genome found!\n" unless -s "$genome.out";
 
-my $size_info = `perl ~/las/git_bin/TElib_benchmark/util/count_base.pl $genome`;
+my $size_info = `perl $script_path/count_base.pl $genome`;
 my ($total, $missing) = (split /\s+/, $size_info)[1,2];
 my $actual = $total - $missing;
 my ($helitron, $tir, $nonltr, $ltr) = (0, 0, 0, 0);
 
 
 #count helitron
-$helitron = `awk '{if (\$11~/Helitron/) print \$5"\\t"\$6"\\t"\$7}' $genome.out |sort -suV|perl ~/las/git_bin/TElib_benchmark/util/count_mask.pl`;
+$helitron = `awk '{if (\$11~/Helitron/) print \$5"\\t"\$6"\\t"\$7}' $genome.out | sort -suV | perl $script_path/count_mask.pl`;
 
 #count non-heli TIRs
-$tir = `awk '{if (\$11~/DNA|MITE/ && \$11!~/Helitron/) print \$5"\\t"\$6"\\t"\$7}' $genome.out |sort -suV|perl ~/las/git_bin/TElib_benchmark/util/count_mask.pl`;
+$tir = `awk '{if (\$11~/DNA|MITE/ && \$11!~/Helitron/) print \$5"\\t"\$6"\\t"\$7}' $genome.out | sort -suV | perl $script_path/count_mask.pl`;
 
 #count SINE and LINE
-$nonltr = `awk '{if (\$11~/SINE|LINE/) print \$5"\\t"\$6"\\t"\$7}' $genome.out |sort -suV|perl ~/las/git_bin/TElib_benchmark/util/count_mask.pl`;
+$nonltr = `awk '{if (\$11~/SINE|LINE/) print \$5"\\t"\$6"\\t"\$7}' $genome.out | sort -suV | perl $script_path/count_mask.pl`;
 
 #count LTR
-$ltr = `awk '{if (\$11~/LTR/) print \$5"\\t"\$6"\\t"\$7}' $genome.out |sort -suV|perl ~/las/git_bin/TElib_benchmark/util/count_mask.pl`;
+$ltr = `awk '{if (\$11~/LTR/) print \$5"\\t"\$6"\\t"\$7}' $genome.out | sort -suV | perl $script_path/count_mask.pl`;
 
 my $hel_frac = sprintf("%.2f%", $helitron/$actual*100);
 my $tir_frac = sprintf("%.2f%", $tir/$actual*100);
