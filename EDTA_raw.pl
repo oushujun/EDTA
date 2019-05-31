046aa956a3321de16b3e0bbb8188fcbc3202aa12
Shujun Ou <shujun.ou.1@gmail.com>
2019-05-31 18:27:25 -0500

    obtain raw TE candidates from many programs

--- EDTA_raw.pl
+++ EDTA_raw.pl
@@ -1,56 +1,122 @@
+#!/usr/bin/perl -w
+use strict;
+use FindBin;
+
+########################################################
+##### Perform initial searches for TE candidates    ####
+##### Shujun Ou (shujun.ou.1@gmail.com, 05/31/2019) ####
+########################################################
+
+## Input:
+#	$genome
+
+## Output:
+#	$genome.LTR.raw.fa
+#	$genome.TIR.raw.fa
+#	$genome.MITE.raw.fa
+#	$genome.Helitron.raw.fa
+
+my $usage = "\nProvide initial filterings for raw TE libraries and generate stage 0 and stage0.HQ datasets
+	perl EDTA_stage0.pl [options]
+		-genome	[File]	The genome FASTA
+		-threads	[int]	Number of theads to run this script
+		-help|-h	Display this help info
+\n";
+
+# pre-defined
+my $genome = '';
+my $threads = 4;
+my $script_path = $FindBin::Bin;
+my $genometools = "$script_path/bin/genometools-1.5.10/bin/gt";
+my $LTR_FINDER = "$script_path/bin/LTR_FINDER_parallel/LTR_FINDER_parallel";
+my $LTR_retriever = "$script_path/bin/LTR_retriever/LTR_retriever";
+my $TIR_Learner = "$script_path/bin/TIR_Learner/TIR_Learner";
+my $MITE_Hunter = "$script_path/bin/MITE-Hunter2/MITE_Hunter_manager.pl";
+my $HelitronScanner = "$script_path/util/run_helitron_scanner.sh";
+
+# read parameters
+my $k=0;
+foreach (@ARGV){
+	$genome = $ARGV[$k+1] if /^-genome$/i and $ARGV[$k+1] !~ /^-/;
+	$threads = $ARGV[$k+1] if /^-threads$/i and $ARGV[$k+1] !~ /^-/;
+	die $usage if /^-help$|^-h$/i;
+	$k++;
+  }
+
+# check files and dependencies
+die "Genome file $genome not exists!\n$usage" unless -s $genome;
+die "The GenomeTools is not found in $genometools!\n" unless -s $genometools;
+die "The LTR_FINDER_parallel is not found in $LTR_FINDER!\n" unless -s $LTR_FINDER;
+die "The LTR_retriever is not found in $LTR_retriever!\n" unless -s $LTR_retriever;
+#die "The TIR_Learner is not found in $TIR_Learner!\n" unless -s $TIR_Learner;
+die "The MITE_Hunter is not found in $MITE_Hunter!\n" unless -s $MITE_Hunter;
+die "The HelitronScanner is not found in $HelitronScanner!\n" unless -s $HelitronScanner;
+
+# Make working directories
+`mkdir $genome.EDTA.raw` unless -e "$genome.EDTA.raw" && -d "$genome.EDTA.raw";
+`mkdir $genome.EDTA.raw/LTR` unless -e "$genome.EDTA.raw/LTR" && -d "$genome.EDTA.raw/LTR";
+`mkdir $genome.EDTA.raw/TIR` unless -e "$genome.EDTA.raw/TIR" && -d "$genome.EDTA.raw/TIR";
+`mkdir $genome.EDTA.raw/MITE` unless -e "$genome.EDTA.raw/MITE" && -d "$genome.EDTA.raw/MITE";
+`mkdir $genome.EDTA.raw/Helitron` unless -e "$genome.EDTA.raw/Helitron" && -d "$genome.EDTA.raw/Helitron";
 
 
 ###########################
 ###### LTR_retriever ######
 ###########################
 
-# run LTRharvest and LTR_FINDER
-~/las/git_bin/TElib_benchmark/bin/genometools-1.5.10/bin/gt suffixerator -db $genome.TIR.fa.stg0 -indexname $genome.TIR.fa.stg0 -tis -suf -lcp -des -ssp -sds -dna
-~/las/git_bin/TElib_benchmark/bin/genometools-1.5.10/bin/gt ltrharvest -index $genome.TIR.fa.stg0 -minlenltr 100 -maxlenltr 7000 -mintsd 4 -maxtsd 6 -motif TGCA -motifmis 1 -similar 85 -vic 10 -seed 20 -seqids yes > $name.harvest.scn
-cp $genome.LTRlib.fa ../$genome.LTR.raw.fa
-cd ../
+# enter the working directory and create genome softlink
+chdir "$genome.EDTA.raw/LTR";
+`ln -s ../../$genome $genome` unless -s $genome;
 
+# run LTRharvest
+`$genometools suffixerator -db $genome -indexname $genome -tis -suf -lcp -des -ssp -sds -dna`;
+`$genometools ltrharvest -index $genome -minlenltr 100 -maxlenltr 7000 -mintsd 4 -maxtsd 6 -motif TGCA -motifmis 1 -similar 85 -vic 10 -seed 20 -seqids yes > $genome.harvest.scn`;
+`rm $genome.esq $genome.lcp $genome.llv $genome.md5 $genome.prj $genome.sds $genome.suf`;
+
+# run LTR_FINDER_parallel
+`perl $LTR_FINDER -seq $genome -threads $threads -harvest_out -size 1000000 -time 300`;
+
+# run LTR_retriever
+`cat $genome.harvest.scn $genome.finder.combine.scn > $genome.rawLTR.scn`;
+`perl $LTR_retriever -genome $genome -inharvest $genome.rawLTR.scn -threads $threads -noanno`;
+`cp $genome.LTRlib.fa ../$genome.LTR.raw.fa`;
 
 
 ###########################
 ######  TIR-Learner  ######
 ###########################
 
-# run TIR-Learner
+# enter the working directory and create genome softlink
+chdir "../TIR";
+`ln -s ../../$genome $genome` unless -s $genome;
 
+# run TIR-Learner
+# TBD
 
 
 ###########################
 ######  MITE-Hunter  ######
 ###########################
 
+# enter the working directory and create genome softlink
+chdir "../MITE";
+`ln -s ../../$genome $genome` unless -s $genome;
+
 # run MITE-Hunter
-mkdir ${genome}_MITE
-cp $genome.LTRlib.fa $genome.LTRlib.fa_MITE
-cd ${genome}_MITE
-ln -s ../$genome $genome
-perl ~/las/git_bin/TElib_benchmark/bin/MITE-Hunter2/MITE_Hunter_manager.pl -l 2 -w 1000 -L 80 -m 1 -S 12345678 -c 16 -i $genome
-cat *_Step8_* > $genome.MITE-Hunter.raw.fa
-cp $genome.MITE-Hunter.raw.fa ../
-cd ..
+`perl $MITE_Hunter -l 2 -w 1000 -L 80 -m 1 -S 12345678 -c $threads -i $genome`;
+`cat *_Step8_* > $genome.MITE.raw.fa`;
+`rm $genome $genome.index $genome.nhr $genome.nin $genome.nsq`;
+`cp $genome.MITE.raw.fa ../$genome.MITE.raw.fa`;
+
 
 #############################
 ###### HelitronScanner ######
 #############################
 
+# enter the working directory and create genome softlink
+chdir "../Helitron";
+`ln -s ../../$genome $genome` unless -s $genome;
+
 # run HelitronScanner
-sh ~/las/git_bin/Plant_TE_annotation/helitron/run_helitron_scanner.sh $genome $threads
-
-# filtre out low-quality Helitron candidates
-perl ~/las/git_bin/TElib_benchmark/util/format_helitronscanner_out.pl $genome
-perl -i -nle 'print $_ and next unless /^>/; my $line=(split)[0]; $line=~s/#SUB_//; print "$line#DNA/Helitron"' $genome.HelitronScanner.filtered.fa
-
-# filter out low-copy Helitron candidates
-
-cp $genome.HelitronScanner.filtered.fa $genome.Helitron.raw.fa
-
-# clean up DNA TE and LINE coding sequence, and plant protein coding sequence
-perl ~/las/git_bin/TElib_benchmark/util/cleanup_proteins.pl $genome.Helitron.raw.fa.renamed
-perl ~/las/git_bin/TElib_benchmark/util/rename_TE.pl $genome.Helitron.raw.fa.renamed.clean.clean > $genome.Helitron.fa.stg0
-
-
+`sh $HelitronScanner $genome $threads`;
+`cp $genome.HelitronScanner.filtered.fa ../$genome.Helitron.raw.fa`;
