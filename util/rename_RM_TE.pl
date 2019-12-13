019a81e68a3175438b8c5a0cee7dc0c4c6210cc3
Zhigui Bao <zhigui.bao@gmail.com>
2019-12-13 21:40:12 +0800

    Create rename_RM_TE.pl

---
+++ util/rename_RM_TE.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -wuse strict;
+
+my $usage = "";
+my $fasta = $ARGV[0];
+
+my %hash=("DNA/hAT"=>"DNA/DTA",
+          "DNA/CACTA"=>"DNA/DTC",
+          "DNA/PIF-Harbinger"=>"DNA/DTH",
+          "DNA/Mutator"=>"DNA/DTM",
+          "DNA/Tcl-Mariner"=>"DNA/DTT",
+          "LTR" => "LTR/Unknown",
+          "RC/Helitron"=>"DNA/Helitron");
+
+
+open FA, "<$fasta" or die "\nInput not found!\n$usage";
+$/ = "\n>";
+my $num = 0;
+my %data;
+while (<FA>){
+        s/>//g;
+        $num = sprintf("%08d", $num);
+        my ($id, $seq) = (split /\n/, $_, 2);
+        my $name = (split /\s/, $id)[0];
+        $seq =~ s/\s+//g;
+        my ($class) = ($1) if $name =~ /^.*#(.*)/;
+        #rename TE as unknown if $class info could not be retrieved
+        #retain LTR-INT info for LTR sequences
+    my $tag = 0;
+    foreach my $key (sort keys %hash){
+        if ($class =~ /$key/i){
+            print ">RM_${num}#$hash{$key}\n$seq\n";
+            #print "$class\t$key\n";
+            $tag = 1;
+            last;
+        }
+    }
+    if ($tag == 0){
+        print ">RM_${num}#$class\n$seq\n";
+    }
+    $num +=1;
+}
+close FA;
