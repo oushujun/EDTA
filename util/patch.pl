c4833bb91c3c28d610099063e6d0399bfd056562
Shujun Ou <shujun.ou.1@gmail.com>
2020-02-01 14:42:34 -0600

    patch to v1.7.4+

--- util/patch.pl
+++ util/patch.pl
@@ -3,7 +3,7 @@ use strict;
 use FindBin;
 use File::Basename;
 
-my $usage = "\n\tPatch EDTA results from v1.6.x to v1.7.x. After this patch, rerun EDTA.pl -step final
+my $usage = "\n\tPatch EDTA results from v1.6.x to v1.7.4+. After this patch, rerun EDTA.pl -step final
 		perl patch.pl genome.fa\n";
 
 ## preset
@@ -15,9 +15,14 @@ my $bed2gff = "$script_path/bed2gff.pl";
 my $gff2bed = "$script_path/gff2bed.pl";
 
 my $genome = $ARGV[0];
+my $genome_ori = $genome;
 
 ## raw/LTR
-chdir "$genome.EDTA.raw/LTR";
+`cp $genome $genome.mod`;
+`mv $genome.EDTA.raw $genome.mod.EDTA.raw`;
+chdir "$genome.mod.EDTA.raw/LTR";
+`for i in \`ls\`; do mv -f \$i \$(echo \$i | perl -nle 's/$genome_ori/$genome_ori.mod/; s/.mod.mod/.mod/; print \$_'); done`;
+$genome = "$genome.mod";
 
 # generate annotated output and gff
 `ln -s ../../$genome`;
@@ -27,6 +32,7 @@ chdir "$genome.EDTA.raw/LTR";
 
 ## raw/TIR
 chdir "../TIR";
+`for i in \`ls\`; do mv -f \$i \$(echo \$i | perl -nle 's/$genome_ori/$genome_ori.mod/; s/.mod.mod/.mod/; print \$_'); done`;
 
 # get gff of intact TIR elements
 `perl -nle 's/\\-\\+\\-/_Len:/; my (\$chr, \$method, \$supfam, \$s, \$e, \$anno) = (split)[0,1,2,3,4,8]; my \$class='DNA'; \$class='MITE' if \$e-\$s+1 <= 600; \$anno = "ID=\$chr:\$s..\$e#\$class/\$supfam;\$anno"; print "\$chr \$method \$class/\$supfam \$s \$e . . . \$anno \$chr:\$s..\$e"' ./TIR-Learner-Result/TIR-Learner_FinalAnn.gff3| perl $output_by_list 10 - 1 $genome.TIR.raw.fa -MSU0 -MSU1 | awk '{\$10=""; print \$0}' | perl -nle 's/\\s+/\\t/g; print \$_' > $genome.TIR.intact.fa.gff`;
@@ -35,6 +41,7 @@ chdir "../TIR";
 
 ## raw/Helitron
 chdir "../Helitron";
+`for i in \`ls\`; do mv -f \$i \$(echo \$i | perl -nle 's/$genome_ori/$genome_ori.mod/; s/.mod.mod/.mod/; print \$_'); done`;
 `perl $make_gff $genome.Helitron.intact.fa`;
 `cp $genome.Helitron.intact.fa $genome.Helitron.intact.fa.gff ../`;
 
@@ -47,4 +54,18 @@ chdir '../';
 `perl $bed2gff $genome.EDTA.intact.bed`;
 `mv $genome.EDTA.intact.bed.gff $genome.EDTA.intact.gff`;
 `cp $genome.EDTA.intact.gff ../`;
+
+## rename filtered
+chdir '../';
+`mv $genome_ori.EDTA.combine $genome.EDTA.combine`;
+chdir "$genome.EDTA.combine";
+`for i in \`ls\`; do mv -f \$i \$(echo \$i | perl -nle 's/$genome_ori/$genome_ori.mod/; s/.mod.mod/.mod/; print \$_'); done`;
+
+## rename final
+chdir '../';
+`mv $genome_ori.EDTA.final $genome.EDTA.final`;
+chdir "$genome.EDTA.final";
+`for i in \`ls\`; do mv -f \$i \$(echo \$i | perl -nle 's/$genome_ori/$genome_ori.mod/; s/.mod.mod/.mod/; print \$_'); done`;
+
+
 
