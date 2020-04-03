b3d5999463ed4432fe09f4f7799fcb84a36432f3
Shujun Ou <shujun.ou.1@gmail.com>
2020-04-03 12:46:58 -0500

    output identity instead of divergence

--- util/make_gff3_with_RMout.pl
+++ util/make_gff3_with_RMout.pl
@@ -13,13 +13,13 @@ chomp ($date);
 open RMout, "sort -sV -k5,5 $ARGV[0]|" or die "ERROR: $!";
 open GFF, ">$ARGV[0].gff" or die "ERROR: $!";
 print GFF "##gff-version 2\n##date $date
-##Chromosome Annotator Repeat_class/superfamily Start End Diversity(%) Strand SW_score Repeat_famliy\n";
+##Chromosome Annotator Repeat_class/superfamily Start End Identity Strand SW_score Repeat_famliy\n";
 
 my %seq_flag;
 my $annotator="EDTA";
 while (<RMout>){
 	s/^\s+//;
-	my ($SW_score, $div, $chr, $chr_len, $element_start, $element_end, $element_length, $left_len, $strand, $TE_ID, $TE_class);
+	my ($SW_score, $div, $iden, $chr, $chr_len, $element_start, $element_end, $element_length, $left_len, $strand, $TE_ID, $TE_class);
 	($SW_score, $div, $chr, $element_start, $element_end, $left_len, $strand, $TE_ID, $TE_class)=(split)[0,1,4,5,6,7,8,9,10];
 	next if $SW_score!~/[0-9]+/;
 	$TE_ID=~s/_INT-int$/_INT/;
@@ -32,7 +32,8 @@ while (<RMout>){
 		$chr_len=$element_end+$left_len;
 		print GFF "##sequence-region $chr 1 $chr_len\n";
 		}
-	print GFF "$chr\t$annotator\t$TE_class\t$element_start\t$element_end\t$div\t$strand\t$SW_score\t$TE_ID\n"
+	$iden = 1 - $div/100;
+	print GFF "$chr\t$annotator\t$TE_class\t$element_start\t$element_end\t$iden\t$strand\t$SW_score\t$TE_ID\n"
 	}
 close RMout;
 close GFF;
