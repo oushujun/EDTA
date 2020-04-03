e1ecb6e58d55e2f22bff5ca963c1dda6cee9f6ed
Shujun Ou <shujun.ou.1@gmail.com>
2020-04-03 12:46:24 -0500

    improve class identification; add identity info for homology-based annotation

--- util/gff2bed.pl
+++ util/gff2bed.pl
@@ -15,8 +15,9 @@ open GFF, "<$ARGV[0]" or die $usage;
 while (<GFF>){
 	chmod;
 	next if /^#/;
-	my ($id, $type, $start, $end, $dir, $info) = (split)[0,2,3,4,6,8];
-	my $class = "LTR";
+	my ($id, $type, $start, $end, $iden, $dir, $info) = (split)[0,2,3,4,5,6,8];
+	my $class = "undef";
+	$class = "LTR" if $type =~ /LTR/i or $type =~ /long_terminal_repeat/i or $type =~ /target_site_duplication/i;
 	$class = "TIR" if $type =~ /DT/ or ($type =~ /DNA|MITE/ and $type !~ /Helitron|DHH/);
 	$class = "Cent" if $type =~ /Cent/i;
 	$class = "knob" if $type =~ /knob/i;
@@ -27,7 +28,7 @@ while (<GFF>){
 	$class = "subtelomere" if $type =~ /subtelomere/i;
 	$class = "Helitron" if $type =~ /Helitron|DHH/i;
 	$class = $1 if $type =~ /^(.*)\/.*/ and $1 !~ /DNA|MITE/i;
-	$info = "ID=$info" if $method eq "homology";
+	$info = "ID=$info;Identity=$iden" if $method eq "homology";
 	$info =~ s/;Method=.*//; #remove method info at the end of the annotation
 	next if $type eq "repeat_region";
 	next unless defined $id and defined $start;
