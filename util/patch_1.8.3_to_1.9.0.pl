932084e8ef8cb2b2b003b27a868e1d4d08701673
Shujun Ou <shujun.ou.1@gmail.com>
2020-07-28 02:02:36 -0500

    add TE summary

--- util/patch_1.8.3_to_1.9.0.pl
+++ util/patch_1.8.3_to_1.9.0.pl
@@ -22,10 +22,10 @@ my $get_frag = "$script_path/get_frag.pl";
 my $keep_nest = "$script_path/keep_nest.pl";
 my $combine_overlap = "$script_path/combine_overlap.pl";
 my $count_base = "$script_path/count_base.pl";
-my $buildSummary = "$script_path/util/buildSummary.pl";
+my $buildSummary = "$script_path/buildSummary.pl";
 
 
-if (0){
+if (1){
 ## LTR
 chdir "$genome.EDTA.raw/LTR";
 
@@ -90,17 +90,15 @@ chdir "../";
 chdir "$genome.EDTA.anno";
 `ln -s ../$genome $genome` unless -e $genome;
 
-if (0){
 `cp ../$genome.EDTA.final/$genome.EDTA.intact.gff3 ./`;
 `perl $RMout2bed $genome.EDTA.RM.out > $genome.EDTA.RM.bed`;
 `perl $bed2gff $genome.EDTA.RM.bed TE_homo > $genome.EDTA.RM.gff3`;
 `perl $gff2bed $genome.EDTA.intact.gff3 structural > $genome.EDTA.intact.bed`;
 `perl $gff2bed $genome.EDTA.RM.gff3 homology > $genome.EDTA.RM.bed`;
 `perl $combine_overlap $genome.EDTA.intact.bed $genome.EDTA.intact.bed.cmb 5`;
-#`perl $combine_overlap $genome.EDTA.RM.bed $genome.EDTA.RM.bed.cmb 5`;
 `perl $get_frag $genome.EDTA.RM.bed $genome.EDTA.intact.bed.cmb $threads`;
 `perl $keep_nest $genome.EDTA.intact.bed $genome.EDTA.RM.bed $threads`;
-`sort -suV $genome.EDTA.intact.bed-$genome.EDTA.RM.bed $genome.EDTA.RM.bed-$genome.EDTA.intact.bed.cmb > $genome.EDTA.TEanno.bed`;
+#`sort -suV $genome.EDTA.intact.bed-$genome.EDTA.RM.bed $genome.EDTA.RM.bed-$genome.EDTA.intact.bed.cmb > $genome.EDTA.TEanno.bed`;
 `grep homology $genome.EDTA.intact.bed-$genome.EDTA.RM.bed > $genome.EDTA.intact.bed-$genome.EDTA.RM.bed.homo`;
 `sort -suV $genome.EDTA.intact.bed-$genome.EDTA.RM.bed.homo $genome.EDTA.RM.bed-$genome.EDTA.intact.bed.cmb > $genome.EDTA.homo.bed`;
 `perl $bed2gff $genome.EDTA.homo.bed TE_homo > $genome.EDTA.homo.gff3`;
@@ -108,18 +106,12 @@ if (0){
 `grep -v '^#' $genome.EDTA.TEanno.gff3.raw | sort -sV -k1,1 -k4,4 | perl -0777 -ne '\$date=\`date\`; \$date=~s/\\s+\$//; print "##gff-version 3\\n##date \$date\\n##Identity: Sequence identity (0-1) between the library sequence and the target region.\\n##ltr_identity: Sequence identity (0-1) between the left and right LTR regions.\\n##tsd: target site duplication.\\n##seqid source sequence_ontology start end score strand phase attributes\\n\$_"' - > $genome.EDTA.TEanno.gff3`;
 `rm $genome.EDTA.TEanno.gff3.raw`;
 `cp $genome.EDTA.TEanno.gff3 ../`;
-chdir "../";
-}
-#exit;
 
 # make summary table for the annotation
 my $genome_info = `perl $count_base $genome`;
 my ($genome_size, $seq_count) = ((split /\s+/, $genome_info)[1] - (split /\s+/, $genome_info)[2], (split /\s+/, $genome_info)[4]);
 `perl $gff2bed $genome.EDTA.TEanno.gff3 structural > $genome.EDTA.TEanno.bed`;
 `perl -nle 'my (\$chr, \$s, \$e, \$anno, \$dir, \$supfam)=(split)[0,1,2,3,8,12]; print "10000 0.001 0.001 0.001 \$chr \$s \$e NA \$dir \$anno \$supfam"' $genome.EDTA.TEanno.bed > $genome.EDTA.TEanno.out`;
-
-#`perl -nle 'next if /^#/; my (\$chr, undef, \$supfam, \$s, \$e, undef, \$dir, undef, \$anno)=(split); next if \$supfam=~/target_site_duplication|long_terminal_repeat/i; \$anno=\$1 if \$anno=~/Name=(.*?);/; print "10000 0.001 0.001 0.001 \$chr \$s \$e NA \$dir \$anno \$supfam"' $genome.EDTA.TEanno.gff3 > $genome.EDTA.TEanno.out`;
-
-print "$genome_size\t$seq_count\n";
 `perl $buildSummary -maxDiv 40 -genome_size $genome_size -seq_count $seq_count $genome.EDTA.TEanno.out > $genome.EDTA.TEanno.sum 2>/dev/null`;
 
+chdir "../";
