7804e94d1093a44fade6aa0fa147aea510a29138
Shujun Ou <shujun.ou.1@gmail.com>
2020-10-17 09:30:22 -0500

    fix a bug for --cds; improve path parsing

--- EDTA.pl
+++ EDTA.pl
@@ -222,17 +222,20 @@ die "The script output_by_list.pl is not found in $output_by_list!\n" unless -s 
 
 # GenomeTools
 chomp ($genometools=`which gt 2>/dev/null`) if $genometools eq '';
+$genometools =~ s/\s+$//;
 $genometools = dirname($genometools) unless -d $genometools;
 $genometools="$genometools/" if $genometools ne '' and $genometools !~ /\/$/;
 die "Error: gt is not found in the genometools path $genometools!\n" unless -X "${genometools}gt";
 # LTR_retriever
 chomp ($LTR_retriever=`which LTR_retriever 2>/dev/null`) if $LTR_retriever eq '';
+$LTR_retriever =~ s/\s+$//;
 $LTR_retriever = dirname($LTR_retriever) unless -d $LTR_retriever;
 $LTR_retriever="$LTR_retriever/" if $LTR_retriever ne '' and $LTR_retriever !~ /\/$/;
 die "Error: LTR_retriever is not found in the LTR_retriever path $LTR_retriever!\n" unless -X "${LTR_retriever}LTR_retriever";
 # RepeatMasker
 my $rand=int(rand(1000000));
 chomp ($repeatmasker=`which RepeatMasker 2>/dev/null`) if $repeatmasker eq '';
+$repeatmasker =~ s/\s+$//;
 $repeatmasker = dirname($repeatmasker) unless -d $repeatmasker;
 $repeatmasker="$repeatmasker/" if $repeatmasker ne '' and $repeatmasker !~ /\/$/;
 die "Error: RepeatMasker is not found in the RepeatMasker path $repeatmasker!\n" unless -X "${repeatmasker}RepeatMasker";
@@ -242,11 +245,13 @@ die "Error: The RMblast engine is not installed in RepeatMasker!\n" unless $RM_t
 `rm dummy060817.fa.$rand* 2>/dev/null`;
 # RepeatModeler
 chomp ($repeatmodeler=`which RepeatModeler 2>/dev/null`) if $repeatmodeler eq '';
+$repeatmodeler =~ s/\s+$//;
 $repeatmodeler = dirname($repeatmodeler) unless -d $repeatmodeler;
 $repeatmodeler="$repeatmodeler/" if $repeatmodeler ne '' and $repeatmodeler !~ /\/$/;
 die "Error: RepeatModeler is not found in the RepeatModeler path $repeatmodeler!\n" unless -X "${repeatmodeler}RepeatModeler";
 # makeblastdb, blastn, blastx
 chomp ($blastplus=`which makeblastdb 2>/dev/null`) if $blastplus eq '';
+$blastplus =~ s/\s+$//;
 $blastplus = dirname($blastplus) unless -d $blastplus;
 $blastplus="$blastplus/" if $blastplus ne '' and $blastplus !~ /\/$/;
 die "Error: makeblastdb is not found in the BLAST+ path $blastplus!\n" unless -X "${blastplus}makeblastdb";
@@ -254,11 +259,13 @@ die "Error: blastn is not found in the BLAST+ path $blastplus!\n" unless -X "${b
 die "Error: blastx is not found in the BLAST+ path $blastplus!\n" unless -X "${blastplus}blastx";
 # TEsorter
 chomp ($TEsorter=`which TEsorter 2>/dev/null`) if $TEsorter eq '';
+$TEsorter =~ s/\s+$//;
 $TEsorter = dirname($TEsorter) unless -d $TEsorter;
 $TEsorter="$TEsorter/" if $TEsorter ne '' and $TEsorter !~ /\/$/;
 die "Error: TEsorter is not found in the TEsorter path $TEsorter!\n" unless -X "${TEsorter}TEsorter";
 # mdust
 chomp ($mdust=`which mdust 2>/dev/null`) if $mdust eq '';
+$mdust =~ s/\s+$//;
 $mdust = dirname($mdust) unless -d $mdust;
 $mdust = "$mdust/" if $mdust ne '' and $mdust !~ /\/$/;
 die "Error: mdust is not found in the mdust path $mdust!\n" unless -X "${mdust}mdust";
@@ -533,6 +540,8 @@ if ($cds ne ''){
 			`perl -nle 'my \$id = \$1 if /=(repeat_region[0-9]+);/; print "Parent\t\$id\nName\t\$id" if defined \$id' $genome.EDTA.intact.gff3.removed >> $genome.EDTA.intact.fa.raw.masked.cleanup.rmlist`;
 			`perl $filter_gff $genome.EDTA.intact.gff3 $genome.EDTA.intact.fa.raw.masked.cleanup.rmlist > $genome.EDTA.intact.gff3.new`;
 			`mv $genome.EDTA.intact.gff3.new $genome.EDTA.intact.gff3`; #update intact.gff
+			} else {
+			`cp $genome.EDTA.intact.fa.rmCDS $genome.EDTA.intact.fa`;
 			}
 		} else {
 		print STDERR "\t\t\t\tWarning: No CDS left after clean up ($cds.code.noTE empty). Will not clean CDS in the raw lib.\n\n";
