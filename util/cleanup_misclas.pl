0d3ef01ebf4bf5c2a5ec120628e0f627e272b0a9
oushujun <shujun.ou.1@gmail.com>
2023-07-18 14:36:48 -0400

    support TEsorter results to remove misclassified candidates

--- util/cleanup_misclas.pl
+++ util/cleanup_misclas.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
+# Shujun Ou (shujun.ou.1@gmail.com)
+# v1	05/03/2023
 
 my $usage = "\nFilter sequence based on TEsorter classifications. Unclassified sequences will also be output to the clean file.
 	Usage: perl cleanup_misclas.pl sequence.fa.rexdb.cls.tsv
@@ -18,14 +20,23 @@ open Seq, "<$seq" or die $!;
 open Clas, "<$clas" or die $!;
 open Cln_clas, ">$seq.cln.list" or die $!;
 open Cln_seq, ">$seq.cln" or die $!;
+open Dirt_clas, ">$seq.dirt.list" or die $!;
 
 # translate terms
-my %lib = ('DNA', 'DNA', 'MITE', 'DNA', 'Helitron', 'DNA', 'Maverick', 'DNA', 'pararetrovirus', 'DNA', 'TIR', 'DNA', 'MITE', 'DNA', 'LTR', 'LTR', 'Copia', 'Copia', 'Gypsy', 'Gypsy', 'LINE', 'LINE', 'SINE', 'SINE', 'EnSpm_CACTA', 'DTC', 'MuDR_Mutator', 'DTM', 'PIF_Harbinger', 'DTH', 'Tc1_Mariner', 'DTT', 'hAT', 'DTA', 'mixture', 'mixture', 'unknown', 'unknown');
+# Translaste specialized/alia -> general TE class
+my %lib = ('DNA', 'DNA', 'MITE', 'DNA', 'Helitron', 'DNA', 'RC', 'DNA', 'Maverick', 'DNA', 
+	'pararetrovirus', 'DNA', 'TIR', 'DNA', 'MITE', 'DNA', 'Retrovirus', 'Gypsy',
+	'LTR', 'LTR', 'DIRS', 'LTR', 'Copia', 'Copia', 'Gypsy', 'Gypsy', 'LINE', 'LINE', 
+	'Penelope', 'LINE', 'LINE-1', 'LINE','SINE', 'SINE', 'SINE?', 'SINE', 'SINE1', 'SINE', 
+	'SINE2', 'SINE', 'SINE3', 'SINE', 'EnSpm_CACTA', 'DTC', 'MuDR_Mutator', 'DTM', 
+	'PIF_Harbinger', 'DTH', 'Tc1_Mariner', 'DTT', 'hAT', 'DTA', 'mixture', 'mixture', 
+	'Unassigned', 'unknown', 'unknown', 'unknown', 'Unknown', 'unknown', 'repeat', 'unknown',
+	'Satellite', 'Satellite', 'snRNA', 'snRNA');
 
 # read sequence
 $/ = "\n>";
 my %seq;
-my %extra;
+my %extra; # store sequence ID annotation
 while (<Seq>){
 	s/>//g;
 	my ($id, $seq) = (split /\n/, $_, 2);
@@ -39,17 +50,19 @@ while (<Seq>){
 close Seq;
 $/ = "\n";
 
-# filter out mis-classified
+# filter out mis-classified based on TEsorter results
 while (<Clas>){
 	chomp;
 	(print Cln_clas "$_\n" and next) if /^#/;
 	my ($id, $order, $superfam) = (split)[0,1,2];
-	my ($ori_cla, $ori_superfam) = ($1, $2) if $id =~ /#(.*)\/(.*)$/;
-	delete $seq{$id} and next if $ori_superfam ne $lib{$superfam} and $superfam ne 'unknown';
-	delete $seq{$id} and next if $lib{$ori_cla} ne $lib{$order} and $superfam eq 'unknown';
-	delete $seq{$id} and next if $ori_superfam ne 'Helitron' and $order eq 'Helitron';
-	delete $seq{$id} and next if $ori_superfam ne 'pararetrovirus' and $order eq 'pararetrovirus';
-	delete $seq{$id} and next if $ori_superfam ne 'Maverick' and $order eq 'Maverick';
+	my ($ori_order, $ori_superfam) = ('unknown', 'unknown');
+	($ori_order, $ori_superfam) = ($1, $2) if $id =~ /#(.*)\/(.*)$/;
+	$ori_order = $1 if $id =~ /#(.*)$/ and $ori_order eq 'unknown';
+	(delete $seq{$id} and print Dirt_clas "$_\n" and next) if $lib{$ori_order} ne $lib{$order} and $lib{$ori_order} ne 'unknown';
+	(delete $seq{$id} and print Dirt_clas "$_\n" and next) if $ori_superfam ne 'Helitron' and $order eq 'Helitron';
+	(delete $seq{$id} and print Dirt_clas "$_\n" and next) if $ori_superfam eq 'Helitron' and $order ne 'Helitron';
+	(delete $seq{$id} and print Dirt_clas "$_\n" and next) if $ori_superfam ne 'pararetrovirus' and $order eq 'pararetrovirus';
+	(delete $seq{$id} and print Dirt_clas "$_\n" and next) if $ori_superfam ne 'Maverick' and $order eq 'Maverick';
 	my $extra = 'NA';
 	$extra = $extra{$id} if defined $extra{$id};
 	if ($extra eq 'NA'){
@@ -57,20 +70,34 @@ while (<Clas>){
 		} else {
 		$extra = "\t$extra";
 		}
-	print Cln_seq ">${id}$extra\n$seq{$id}\n" and delete $seq{$id};
-	print Cln_clas "$_\n";
+	if (defined $seq{$id}){
+		print Cln_seq ">${id}$extra\n$seq{$id}\n" and delete $seq{$id};
+		print Cln_clas "$_\n";
+		}
 	}
 
 # output non-classified
 foreach my $id (keys %seq){
-	print Cln_seq ">$id\n$seq{$id}\n";
-	my ($ori_cla, $ori_superfam) = ($1, $2) if $id =~ /#(.*)\/(.*)$/;
-	$ori_cla = "TIR" if $ori_superfam =~ /^DT/;
-	($ori_cla, $ori_superfam) = ("Helitron", "unknown") if $ori_superfam eq 'Helitron';
-	print Cln_clas "$id\t$ori_cla\t$ori_superfam\tunknown\tnone\t?\tnone\n";
+	my $extra = 'NA';
+	$extra = $extra{$id} if defined $extra{$id};
+	if ($extra eq 'NA'){
+		$extra = '';
+		} else {
+		$extra = "\t$extra";
+		}
+	print Cln_seq ">${id}$extra\n$seq{$id}\n";
+	my ($ori_order, $ori_superfam) = ("Unknown", "Unknown");
+	if ($id =~ /#(.*)\/(.*)$/){
+		($ori_order, $ori_superfam) = ($1, $2);
+		} elsif ($id =~ /#(.*)$/){
+		($ori_order, $ori_superfam) = ($1, "Unknown");
+		}
+	$ori_order = "TIR" if $ori_superfam =~ /^DT/;
+	($ori_order, $ori_superfam) = ("Helitron", "unknown") if $ori_superfam eq 'Helitron';
+	print Cln_clas "$id\t$ori_order\t$ori_superfam\tunknown\tnone\t?\tnone\n";
 	}
 
 close Clas;
 close Cln_clas;
 close Cln_seq;
-
+close Dirt_clas;
