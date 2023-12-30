6ded66974104a7a8010e0d399e9af9ace9970f38
oushujun <shujun.ou.1@gmail.com>
2023-12-29 20:37:10 -0500

    a simple code to convert gff3 format to RepeatMasker out format

---
+++ util/gff2RMout.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+# Shujun Ou (shujun.ou.1@gmail.com)
+# Facilitated by ChatGPT
+#
+# usage: perl gff2RMout.pl genome.gff3 genome.out
+
+# Open the GFF3 file for reading
+open(my $gff3_file, "<", $ARGV[0]) or die "Cannot open input.gff3: $!";
+
+# Open the RepeatMasker .out file for writing
+open(my $rm_out_file, ">", $ARGV[1]) or die "Cannot open output.out: $!";
+
+# Read and process the GFF3 file line by line
+while (my $line = <$gff3_file>) {
+    chomp $line;
+
+    # Skip header lines or comments
+    next if $line =~ /^#/;
+
+    # Split the line into fields
+    my @fields = split /\t/, $line;
+
+    # Extract necessary information (adjust indices based on your GFF3 structure)
+    my $seqName = $fields[0];
+    my $source = $fields[1];
+    my $featureType = $fields[2];
+    my $start = $fields[3];
+    my $end = $fields[4];
+    my $score = $fields[5];
+    my $strand = $fields[6];
+    my $phase = $fields[7];
+    my $attributes = $fields[8];
+
+    # Format the output for RepeatMasker .out (modify according to the expected format)
+    printf $rm_out_file "0 0 0 0\t$seqName\t$start\t$end\tNA\t$strand\tLINE\tLINE\tNA NA NA NA\n";
+}
+
+# Close the filehandles
+close($gff3_file);
+close($rm_out_file);
+
