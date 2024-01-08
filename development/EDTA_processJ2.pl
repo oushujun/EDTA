12f3b80afb54bc5d6cff63773e727dbeea63ebb5
oushujun <shujun.ou.1@gmail.com>
2024-01-08 14:35:00 -0500

    Expired filtering schemes

--- development/EDTA_processJ2.pl
+++ development/EDTA_processJ2.pl
@@ -1,25 +1,28 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
+use warnings;
 use strict;
 use FindBin;
 use File::Basename;
 
-#######################################################################
-##### Perform EDTA basic and advcanced filterings on TE candidates ####
-##### Shujun Ou (shujun.ou.1@gmail.com, 11/04/2019)                ####
-#######################################################################
+#####################################################################
+##### Perform EDTA basic and advance filtering on TE candidates #####
+##### Shujun Ou (shujun.ou.1@gmail.com, 11/04/2019)             #####
+#####################################################################
 
 ## Input:
 #	$genome.LTR.raw.fa
+#	$genome.nonLTR.raw.fa
 #	$genome.TIR.raw.fa
 #	$genome.Helitron.raw.fa
 
 ## Output:
-#	$genome.LTR.TIR.Helitron.fa.stg1
+#	$genome.EDTA.fa.stg1
 
-my $usage = "\nPerform EDTA basic and advcanced filterings for raw TE candidates and generate the stage 1 library
+my $usage = "\nPerform EDTA basic and advance filtering for raw TE candidates and generate the stage 1 library
 	perl EDTA_processF.pl [options]
 		-genome	[File]	The genome FASTA
 		-ltr	[File]	The raw LTR library FASTA
+		-nonltr	[File]	The raw nonLTR library FASTA
 		-tir	[File]	The raw TIR library FASTA
 		-helitron	[File]	The raw Helitron library FASTA
 		-mindiff_ltr	[float]	The minimum fold difference in richness between LTRs and contaminants (default: 1)
@@ -37,8 +40,10 @@ my $usage = "\nPerform EDTA basic and advcanced filterings for raw TE candidates
 # user input
 my $genome = '';
 my $LTRraw = '';
+my $nonLTRraw = '';
 my $TIRraw = '';
 my $Helitronraw = '';
+my $err = '';
 
 # pre-defined
 my $mindiff_LTR = 1; #minimum richness difference between $TE1 and $TE2 for a sequence to be considered as real to $TE1
@@ -52,9 +57,7 @@ my $rename_TE = "$script_path/util/rename_TE.pl";
 my $cleanup_tandem = "$script_path/util/cleanup_tandem.pl";
 my $cleanup_nested = "$script_path/util/cleanup_nested.pl";
 my $cleanup_proteins = "$script_path/util/cleanup_proteins.pl";
-my $output_by_list = "$script_path/util/output_by_list.pl";
 my $protlib = "$script_path/database/alluniRefprexp082813";
-my $HelitronScanner = "$script_path/util/run_helitron_scanner.sh";
 my $repeatmasker = " ";
 my $blast = " ";
 
@@ -63,6 +66,7 @@ my $k=0;
 foreach (@ARGV){
 	$genome = $ARGV[$k+1] if /^-genome$/i and $ARGV[$k+1] !~ /^-/;
 	$LTRraw = $ARGV[$k+1] if /^-ltr$/i and $ARGV[$k+1] !~ /^-/;
+	$nonLTRraw = $ARGV[$k+1] if /^-nonltr$/i and $ARGV[$k+1] !~ /^-/;
 	$TIRraw = $ARGV[$k+1] if /^-tir/i and $ARGV[$k+1] !~ /^-/;
 	$Helitronraw = $ARGV[$k+1] if /^-helitron/i and $ARGV[$k+1] !~ /^-/;
 	$mindiff_LTR = $ARGV[$k+1] if /^-mindiff_ltr/i and $ARGV[$k+1] !~ /^-/;
@@ -76,9 +80,13 @@ foreach (@ARGV){
 	$k++;
         }
 
+# define RepeatMasker -pa parameter
+my $rm_threads = int($threads/4);
+
 # check files and dependencies
 die "Genome file $genome not exists!\n$usage" unless -s $genome;
 die "LTR raw library file $LTRraw not exists!\n$usage" unless -s $LTRraw;
+die "nonLTR raw library file $nonLTRraw not exists!\n$usage" unless -e $nonLTRraw; # allow empty file
 die "TIR raw library file $TIRraw not exists!\n$usage" unless -s $TIRraw;
 die "Helitron raw library file $Helitronraw not exists!\n$usage" unless -s $Helitronraw;
 die "The script TE_purifier.pl is not found in $TE_purifier!\n" unless -s $TE_purifier;
@@ -94,21 +102,21 @@ my $genome_file = basename($genome);
 $genome = $genome_file;
 
 # Make working directories
-#`mkdir $genome.EDTA.combine` unless -e "$genome.EDTA.combine" && -d "$genome.EDTA.combine";
-`mkdir $genome.EDTA.combineJ2` unless -e "$genome.EDTA.combineJ2" && -d "$genome.EDTA.combineJ2";
+`mkdir $genome.EDTA.combine` unless -e "$genome.EDTA.combine" && -d "$genome.EDTA.combine";
 
 # enter the combine folder for EDTA processing
-#chdir "$genome.EDTA.combine";
-chdir "$genome.EDTA.combineJ2";
-`ln -s ../$LTRraw $genome.LTR.raw.fa` unless -s "$genome.LTR.raw.fa";
-`ln -s ../$TIRraw $genome.TIR.raw.fa` unless -s "$genome.TIR.raw.fa";
-`ln -s ../$Helitronraw $genome.Helitron.raw.fa` unless -s "$genome.Helitron.raw.fa";
+chdir "$genome.EDTA.combine";
+`cp ../$LTRraw $genome.LTR.raw.fa` unless -s "$genome.LTR.raw.fa";
+`cp ../$nonLTRraw $genome.nonLTR.raw.fa` unless -s "$genome.nonLTR.raw.fa";
+`cp ../$TIRraw $genome.TIR.raw.fa` unless -s "$genome.TIR.raw.fa";
+`cp ../$Helitronraw $genome.Helitron.raw.fa` unless -s "$genome.Helitron.raw.fa";
 
 
 ##################################
 ######  define subroutines  ######
 ##################################
 
+# purify $TE2 contaminants in $TE1
 sub Purifier() {
 	my ($TE1, $TE2, $mindiff) = ($_[0], $_[1], $_[2]);
 	`perl $TE_purifier -TE1 $TE1 -TE2 $TE2 -t $threads -mindiff $mindiff`;
@@ -117,73 +125,64 @@ sub Purifier() {
 
 
 #################################
-###### Advanced filterings ######
+###### Advance filtering ######
 #################################
 
 ## predefined variables
 my $LTR = "$genome.LTR.raw.fa";
+my $nonLTR = "$genome.nonLTR.raw.fa";
 my $TIR = "$genome.TIR.raw.fa";
 my $HEL = "$genome.Helitron.raw.fa";
 
-## prefilter TIRs with LTR regions and HelitronScanner
-# extract LTR regions as HQ
-`grep \"_LTR\" $LTR > $LTR.list`;
-`perl $output_by_list 1 $LTR 1 $LTR.list -FA > $LTR.LTR`;
-
-# identify Helitron contaminants with HelitronScanner
-`sh $HelitronScanner $TIR $threads`;
-`cat $TIR.HelitronScanner.draw.hel.fa $TIR.HelitronScanner.draw.rc.hel.fa | perl -nle \'print \$_ and next unless /^>/; my \$line=(split)[0]; \$line=~s/\#SUB_//; \$line=~s/[\#\\/]/_/g; print \"\$line\#DNA\/Helitron\"\' > $TIR.helitron`;
-`cat $script_path/database/HelitronScanner.training.set.fa >> $TIR.helitron`;
-
-# remove potential LTR and helitron contaminants
-`cat $LTR.LTR $TIR.helitron > $LTR.LTR_$TIR.helitron`;
-`${repeatmasker}RepeatMasker -pa $threads -q -no_is -norna -nolow -div 40 -lib $LTR.LTR_$TIR.helitron $TIR 2>/dev/null`;
-`perl $cleanup_tandem -misschar N -nc 50000 -nr 0.9 -minlen 80 -minscore 3000 -trf 0 -cleanN 1 -cleanT 1 -f $TIR.masked > $TIR.HQ`;
-
-## prefilter Helitrons with LTR regions
-`${repeatmasker}RepeatMasker -pa $threads -q -no_is -norna -nolow -div 40 -lib $LTR.LTR $HEL 2>/dev/null`;
-`perl $cleanup_tandem -misschar N -nc 50000 -nr 0.9 -minlen 100 -minscore 3000 -trf 0 -cleanN 1 -cleanT 1 -f $HEL.masked > $HEL.pre`;
-# remove LTR and TIR in Helitron candidates
-#`cat $genome.LTR.fa.stg0.HQ $genome.TIR.fa.stg0.HQ > $genome.LTR.TIR.fa.stg0.HQ`;
-#`${repeatmasker}RepeatMasker -pa $threads -q -no_is -norna -nolow -div 40 -lib $genome.LTR.TIR.fa.stg0.HQ $genome.Helitron.fa.stg0 2>/dev/null`;
-#`perl $cleanup_tandem -misschar N -nc 50000 -nr 0.9 -minlen 100 -minscore 3000 -trf 0 -cleanN 1 -cleanT 1 -f $genome.Helitron.fa.stg0.masked > $genome.Helitron.fa.stg1`;
-
 ## Purge contaminants
-if (0){
 # purify LTR
-&Purifier("$LTR", "$TIR.HQ", $mindiff_LTR);
+&Purifier("$LTR", "$TIR", $mindiff_LTR);
 &Purifier("$LTR.HQ", "$HEL", $mindiff_LTR);
 `mv $LTR.HQ.HQ $LTR.HQ`;
-}
 
 # purify Helitron
-&Purifier("$HEL.pre", "$TIR.HQ", $mindiff_HEL);
-&Purifier("$HEL.pre.HQ", "$LTR", $mindiff_HEL);
-`mv $HEL.pre.HQ.HQ $HEL.HQ`;
+&Purifier("$HEL", "$TIR", $mindiff_HEL);
+&Purifier("$HEL.HQ", "$LTR", $mindiff_HEL);
+`mv $HEL.HQ.HQ $HEL.HQ`;
 
-if(0){
 # purify TIR
-#&Purifier("$TIR", "$LTR", $mindiff_TIR);
-&Purifier("$TIR.HQ", "$HEL.HQ", $mindiff_TIR);
+&Purifier("$TIR", "$LTR", $mindiff_TIR);
+&Purifier("$TIR.HQ", "$HEL", $mindiff_TIR);
 `mv $TIR.HQ.HQ $TIR.HQ`;
-}
 
-## clean LTRs in TIRs and Helitrons
-#`cat $TIR.HQ $HEL.HQ | perl -nle 's/>/\\n>/g unless /^>/; print \$_' > $genome.TIR.Helitron.fa.stg1.raw`;
-#`${repeatmasker}RepeatMasker -pa $threads -q -no_is -norna -nolow -div 40 -lib $LTR.HQ $genome.TIR.Helitron.fa.stg1.raw 2>/dev/null`;
-#`perl $cleanup_tandem -misschar N -nc 50000 -nr 0.9 -minlen 80 -minscore 3000 -trf 0 -cleanN 1 -cleanT 1 -f $genome.TIR.Helitron.fa.stg1.raw.masked > $genome.TIR.Helitron.fa.stg1.raw.cln`;
-`cat $TIR.HQ $HEL.HQ | perl -nle 's/>/\\n>/g unless /^>/; print \$_' > $genome.TIR.Helitron.fa.stg1.raw.cln`;
+
+# clean nonLTRs in LTRs
+if (-s "$genome.nonLTR.raw.fa"){
+	$err = `${repeatmasker}RepeatMasker -e ncbi -pa $rm_threads -q -no_is -norna -nolow -div 40 -lib $genome.nonLTR.raw.fa $LTR 2>&1`;
+	if ($err !~ /done/) {
+        	`cp $LTR $LTR.masked` if $err =~ s/^.*(No repetitive sequences were detected.*)\s+$/Warning: $1/s;
+	        print STDERR "\n$err\n";
+        	}
+	`perl $cleanup_tandem -misschar N -nc 50000 -nr 0.9 -minlen 80 -minscore 3000 -trf 0 -cleanN 1 -cleanT 1 -f $LTR.masked > $LTR.cln`;
+	} else {
+		`cp $LTR $LTR.cln`;
+	}
+
+
+## clean LTRs and nonLTRs in TIRs and Helitrons
+`cat $TIR.HQ $HEL.HQ | perl -nle 's/>/\\n>/g unless /^>/; print \$_' > $genome.TIR.Helitron.fa.stg1.raw`;
+`cat $LTR.HQ $genome.nonLTR.raw.fa > $genome.LTR.nonLTR.fa`;
+$err = `${repeatmasker}RepeatMasker -e ncbi -pa $rm_threads -q -no_is -norna -nolow -div 40 -lib $genome.LTR.nonLTR.fa $genome.TIR.Helitron.fa.stg1.raw 2>&1`;
+if ($err !~ /done/) {
+	`cp $genome.TIR.Helitron.fa.stg1.raw $genome.TIR.Helitron.fa.stg1.raw.masked` if $err =~ s/^.*(No repetitive sequences were detected.*)\s+$/Warning: $1/s;
+	print STDERR "\n$err\n";
+	}
+`perl $cleanup_tandem -misschar N -nc 50000 -nr 0.9 -minlen 80 -minscore 3000 -trf 0 -cleanN 1 -cleanT 1 -f $genome.TIR.Helitron.fa.stg1.raw.masked > $genome.TIR.Helitron.fa.stg1.raw.cln`;
+
 
 ## cluster TIRs and Helitrons and make stg1 raw library
 `perl $cleanup_nested -in $genome.TIR.Helitron.fa.stg1.raw.cln -threads $threads -minlen 80 -cov 0.95 -blastplus $blast`;
-`cat $LTR $genome.TIR.Helitron.fa.stg1.raw.cln.cln > $genome.LTR.TIR.Helitron.fa.stg1.raw.cln`;
+`cat $LTR.cln $genome.nonLTR.raw.fa $genome.TIR.Helitron.fa.stg1.raw.cln.cln > $genome.EDTA.stg1.raw.cln`;
 
-if (0){
 ## remove protein-coding sequences
-`perl $cleanup_proteins -seq $genome.LTR.TIR.Helitron.fa.stg1.raw.cln -rmdnate 0 -rmline 1 -rmprot 1 -protlib $protlib -blast $blast -threads $threads`;
-`perl $rename_TE $genome.LTR.TIR.Helitron.fa.stg1.raw.cln.clean > $genome.LTR.TIR.Helitron.fa.stg1`;
-}
-`perl $rename_TE $genome.LTR.TIR.Helitron.fa.stg1.raw.cln > $genome.LTR.TIR.Helitron.fa.stg1`; #quick line for testing
+`cp $genome.EDTA.stg1.raw.cln $genome.EDTA.fa.stg1`;
+
+## clean up the folder
+`rm *.masked *.ori.out *.nhr *.nin *.nsq 2>/dev/null`;
 
 chdir '..';
-
