295db65bf3630b3a3096096f0f8c079d58271097
Usman Rashid <usman@smme.edu.pk>
2024-12-12 17:33:00 +1300

    Added REPEATMODELER_POSTPROCESS

---
+++ bin/setup_RepeatModeler_postprocess.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+script_path=$(dirname $0)
+
+cat > input/RepeatModeler_postprocess.pl << EOF
+my \$genome             = 'genome';
+my \$threads            = $1;
+
+my \$cleanup_misclas    = '$script_path/cleanup_misclas.pl';
+my \$cleanup_tandem     = '$script_path/cleanup_tandem.pl';
+my \$output_by_list     = '$script_path/output_by_list.pl';
+
+my \$TEsorter           = ''; # Assumed on PATH
+my \$trf                = '$(which trf)';
+
+EOF
+
+sed -n 567,579p "$script_path/EDTA_raw.pl" \
+    >> input/RepeatModeler_postprocess.pl
+
+sed -n 585,586p "$script_path/EDTA_raw.pl" \
+    >> input/RepeatModeler_postprocess.pl
