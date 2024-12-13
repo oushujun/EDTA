ba29b092faab91373370454f364b0969b9881c39
Usman Rashid <usman@smme.edu.pk>
2024-12-13 15:50:43 +1300

    Added PROCESS_K

--- .github/check_binaries.sh
+++ .github/check_binaries.sh
@@ -10,3 +10,12 @@ if [[ $EDTA_raw_pl_version != $bin_EDTA_raw_pl_version ]]; then
     echo 'Please synchronize bin/EDTA_raw.pl with EDTA_raw.pl'
     exit 1
 fi
+
+EDTA_processK_pl_version=$(md5sum EDTA_processK.pl | cut -f1 -d' ')
+bin_EDTA_processK_pl_version=$(md5sum bin/EDTA_processK.pl | cut -f1 -d' ')
+
+if [[ $EDTA_processK_pl_version != $bin_EDTA_processK_pl_version ]]; then
+    echo 'EDTA_processK.pl != bin/EDTA_processK.pl'
+    echo 'Please synchronize bin/EDTA_processK.pl with EDTA_processK.pl'
+    exit 1
+fi
